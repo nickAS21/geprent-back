1ee27335a00646f8eb17025b4326aab15b2d2d26
Kocherga Vitalii <ko4erha@gmail.com>
2019-04-12 19:18:12 +0300

    Basic application setup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# georent-back
-Backend for Georent project
+#GEORENT
+Backend for GeoRent project
+
+##Building project
+
+From the root dir of a project run
+
+- Windows: 
+```gradlew.bat clean build```
+- Mac, Linux: 
+```./gradlew clean build```
+
+to skip test phase add `-x test` to the previous command.
+
+
---
+++ src/main/java/com/georent/GeoRentStarter.java
@@ -0,0 +1,11 @@
+package com.georent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoRentStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(GeoRentStarter.class, args);
+    }
+}
