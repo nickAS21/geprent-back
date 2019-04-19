3ebc73ad086098486f9c7ade46397c508f88070f
Misha <mishabeheka@gmail.com>
2019-04-19 19:12:16 +0300

    Rename class and delete constructors

---
+++ src/main/java/com/georent/domain/GeoRentUser.java
@@ -0,0 +1,21 @@
+package com.georent.domain;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class GeoRentUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private long phoneNumber;
+}
