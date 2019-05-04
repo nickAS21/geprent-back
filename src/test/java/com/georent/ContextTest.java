c96cecf79c3a77ac67ce19ee849cf02c0673dc06
Kocherga Vitalii <ko4erha@gmail.com>
2019-05-04 14:45:32 +0300

    (feature) configuration changed

--- src/test/java/com/georent/ContextTest.java
+++ src/test/java/com/georent/ContextTest.java
@@ -1,11 +1,7 @@
 package com.georent;
 
-import com.georent.domain.TestObject;
-import com.georent.repository.TestObjectRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -13,14 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 class ContextTest {
 
-    @Autowired
-    private TestObjectRepository repository;
-
     @Test
     void contextSuccessfullyStarted() {
-        TestObject object = new TestObject();
-        object.setId(666L);
-        TestObject save = repository.save(object);
-        Assertions.assertThat(save.getId()).isEqualTo(666);
     }
 }
