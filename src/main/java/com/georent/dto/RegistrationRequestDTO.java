ae977e6ef66a715d523c7491a886b727f4b1321f
nick <nick.@avalr.com.ua>
2019-05-07 17:00:18 +0300

    GEOR-31 - Edit: RegistrationRequestDTO valid email, password; long to string  phoneNumber;

--- src/main/java/com/georent/dto/RegistrationRequestDTO.java
+++ src/main/java/com/georent/dto/RegistrationRequestDTO.java
@@ -2,11 +2,23 @@ package com.georent.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class RegistrationRequestDTO {
     private String firstName;
     private String lastName;
+
+    @Email()
+    @NotBlank()
+    @Size(max = 40)
     private String email;
+
+    @NotBlank()
+    @Size(min = 6, max = 100)
     private String password;
-    private long phoneNumber;
+
+    private String phoneNumber;
 }
