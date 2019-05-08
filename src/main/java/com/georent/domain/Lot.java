b2014eaaeb3f75e29b744a45f5793681d6f25a50
Yamatoirina <yamatoirina@gmail.com>
2019-05-08 16:16:51 +0300

    Entity Lot and Description created

---
+++ src/main/java/com/georent/domain/Lot.java
@@ -0,0 +1,18 @@
+package com.georent.domain;
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+    public class Lot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private  Long userId;
+        private Long addressId;
+        private  Description description;
+}
