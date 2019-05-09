37ff6cb10cb022dc840dea00cad484f7767d6172
nick <nick.@avalr.com.ua>
2019-05-09 20:59:33 +0300

    GEOR-31 - RegistrationRequestDTO valid - add Pattern

--- src/main/java/com/georent/dto/RegistrationRequestDTO.java
+++ src/main/java/com/georent/dto/RegistrationRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
@@ -13,11 +14,13 @@ public class RegistrationRequestDTO {
 
     @Email()
     @NotBlank()
-    @Size(max = 40)
+    @Pattern(regexp="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message="Invalid email address!")
+    @Size(min = 6, max = 40)
     private String email;
 
     @NotBlank()
-    @Size(min = 6, max = 100)
+    @Pattern(regexp="[A-Za-z0-9_-]+", message="Invalid email address!")
+    @Size(min = 8, max = 64)
     private String password;
 
     @Size(max = 12)
