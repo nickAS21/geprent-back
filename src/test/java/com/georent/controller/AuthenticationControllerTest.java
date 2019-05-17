42be1a2a1767f1098f0633611744722f2258205d
nick <nick.@avalr.com.ua>
2019-05-17 20:51:52 +0300

    GEOR-42 edit AuthenticationControllerTest (status.isOk)

--- src/test/java/com/georent/controller/AuthenticationControllerTest.java
+++ src/test/java/com/georent/controller/AuthenticationControllerTest.java
@@ -1,6 +1,10 @@
 package com.georent.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.georent.GeoRentStarter;
+import com.georent.domain.GeoRentUser;
+import com.georent.dto.LoginRequestDTO;
+import com.georent.dto.RegistrationRequestDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +15,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +30,7 @@ public class AuthenticationControllerTest {
     @Autowired
     private AuthenticationController controllerToTest;
 
+    ObjectMapper mapper = new ObjectMapper();
     private MockMvc mockMvc;
 
     @BeforeEach
@@ -33,20 +40,43 @@ public class AuthenticationControllerTest {
     }
 
     @Test
-    public void authenticateUser_mapping_post_login_Return_Status_isBadRequest () throws Exception {
-        mockMvc.perform(post("/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(""))
+    public void authenticateUser_mapping_post_login_Return_Status_ok () throws Exception {
+        LoginRequestDTO reqest = new LoginRequestDTO();
+        reqest.setEmail("mkyong@gmail23.com.aa");
+        reqest.setPassword( "pass5678910");
+        mockMvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(reqest)))
                 .andDo(print())
-                .andExpect(status().isBadRequest())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void registerUser_mapping_post_register_Return_Status_ok () throws Exception {
+        RegistrationRequestDTO reqest = new RegistrationRequestDTO();
+        reqest.setEmail("mkyong@gmail23.com.aa");
+        reqest.setPassword( "pass5678910");
+
+        GeoRentUser user = new GeoRentUser();
+        user.setEmail(reqest.getEmail());
+        user.setFirstName(reqest.getFirstName());
+        mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(reqest)))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andReturn();
     }
 
     @Test
     public void registerUser_mapping_post_register_Return_Status_isBadRequest () throws Exception {
-        mockMvc.perform(post("/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(""))
+        RegistrationRequestDTO reqest = new RegistrationRequestDTO();
+        reqest.setEmail("mkyong@gmail23.com.aa");
+
+        GeoRentUser user = new GeoRentUser();
+        user.setEmail(reqest.getEmail());
+        user.setFirstName(reqest.getFirstName());
+        mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(reqest)))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andReturn();
