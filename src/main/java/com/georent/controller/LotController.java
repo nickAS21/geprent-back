c32fa31a1deed1bd222fae184478096404276dcc
Kocherga Vitalii <ko4erha@gmail.com>
2019-05-23 14:10:20 +0300

    check the difference

--- src/main/java/com/georent/controller/LotController.java
+++ src/main/java/com/georent/controller/LotController.java
@@ -2,20 +2,31 @@ package com.georent.controller;
 
 
 import com.georent.service.LotService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.ResponseEntity.status;
 
+@Slf4j
 @Controller
 @RequestMapping("lot")
 public class LotController {
@@ -37,8 +48,9 @@ public class LotController {
         return status(OK).body(lotService.getLotDto(lotId));
     }
 
-    @PostMapping("/upload")
-    public String setLotUpload(@RequestParam("file") MultipartFile file){
+    //Now it's working. Check the difference.
+    @PostMapping("upload")
+    public ResponseEntity<String> setLotUpload(@RequestParam(name = "file") MultipartFile file){
         String name = "test11";
         if (!file.isEmpty()) {
             try {
@@ -47,12 +59,27 @@ public class LotController {
                         new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
                 stream.write(bytes);
                 stream.close();
-                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+                return ResponseEntity.ok("You successfully uploaded " + name + " into " + name + "-uploaded !");
             } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("You failed to upload " + name + " => " + e.getMessage());
             }
         } else {
-            return "You failed to upload " + name + " because the file was empty.";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You failed to upload " + name + " because the file was empty.");
         }
     }
+
+    //This is more concise variant.
+    @PostMapping("upload-picture")
+    public ResponseEntity<String> uploadPicture(@RequestParam(name = "file") MultipartFile multipartFile){
+        String originalFilename = multipartFile.getOriginalFilename();
+        try(InputStream inputStream = multipartFile.getInputStream()) {
+            Path tempFile = Files.createTempFile("tmp_", originalFilename);
+            Files.copy(inputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            log.error("Unable to save the file", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unable to save the file.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("File saved");
+    }
+
 }
