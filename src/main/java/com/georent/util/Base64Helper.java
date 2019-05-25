634f32a89ae41f0aa0e35f675a327de040e42925
Kocherga Vitalii <ko4erha@gmail.com>
2019-05-25 16:27:54 +0300

    (fix) moved Base64Helper to util package.

---
+++ src/main/java/com/georent/util/Base64Helper.java
@@ -0,0 +1,15 @@
+package com.georent.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
+
+public class Base64Helper {
+
+    public static String encodePictureToBase64(File file) throws IOException {
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+}
