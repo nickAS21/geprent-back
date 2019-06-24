1400c92d9a178e5e6bfd94d6ca4c5b8467b310ff
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-06-24 23:42:27 +0300

    GEOR-55 Specific find and delete methods outlined in RentOrderRepository.

--- src/main/java/com/georent/repository/RentOrderRepository.java
+++ src/main/java/com/georent/repository/RentOrderRepository.java
@@ -4,8 +4,18 @@ import com.georent.domain.RentOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RentOrderRepository extends JpaRepository<RentOrder, Long> {
 
+    List<RentOrder> findByLot_Id(Long lotId);
+
+    List<RentOrder> findByRentee_Id(Long renteeId);
+
+    List<RentOrder> findByLot_IdAndRentee_Id(Long lotId, Long renteeId);
+
+    void deleteAllByLot_Id(Long lotId);
+
+    void deleteAllByRentee_Id(Long renteeId);
+
+    void deleteAllByLot_IdAndRentee_Id(Long lotId, Long renteeId);
 }
