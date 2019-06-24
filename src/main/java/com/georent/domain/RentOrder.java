fc1e4ece056663796a544d35750493df32bbd9cd
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-06-24 22:46:24 +0300

    GEOR-55 Deleted redundant imports for Date & Temporal in RentOrder.

--- src/main/java/com/georent/domain/RentOrder.java
+++ src/main/java/com/georent/domain/RentOrder.java
@@ -14,11 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 @Entity
