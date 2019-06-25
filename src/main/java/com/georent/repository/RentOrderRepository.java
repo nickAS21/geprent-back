d0880237189900af66922726933d2537b67766bb
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-06-25 17:34:34 +0300

    GEOR-55 findByLot_IdAndStatus method outlined in RentOrderRepository.

--- src/main/java/com/georent/repository/RentOrderRepository.java
+++ src/main/java/com/georent/repository/RentOrderRepository.java
@@ -1,6 +1,7 @@
 package com.georent.repository;
 
 import com.georent.domain.RentOrder;
+import com.georent.domain.RentOrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,6 +13,8 @@ public interface RentOrderRepository extends JpaRepository<RentOrder, Long> {
     List<RentOrder> findByRentee_Id(Long renteeId);
 
     List<RentOrder> findByLot_IdAndRentee_Id(Long lotId, Long renteeId);
+
+    List<RentOrder> findByLot_IdAndStatus(Long lotId, RentOrderStatus status);
 
     void deleteAllByLot_Id(Long lotId);
 
