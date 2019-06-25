7f1252940271a022bc6bb0093d1406fc78fe3ec3
nick <nick.@avalr.com.ua>
2019-06-25 11:47:21 +0300

    GEOR-serch-enjine - change in fuzzyLotNameAndAddressSearch: set to map -> set

--- src/main/java/com/georent/dto/LotPageable.java
+++ src/main/java/com/georent/dto/LotPageable.java
@@ -9,9 +9,11 @@ public class LotPageable {
 
     List<LotPageDTO> lots;
     int pageNumber;
+    int totalPages;
 
-    public LotPageable(List<LotPageDTO> lots, int pageNumber) {
+    public LotPageable(List<LotPageDTO> lots, int pageNumber, int totalPages) {
         this.lots = lots;
         this.pageNumber = pageNumber;
+        this.totalPages = totalPages;
     }
 }
