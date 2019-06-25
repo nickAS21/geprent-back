7b315c46d0baab18976d6ce032f2073e0d3579d9
nick <nick.@avalr.com.ua>
2019-06-25 18:20:45 +0300

    GEOR-serch-enjine - add SearchControllerTest

--- src/test/java/com/georent/service/DescriptionSearchServiceTest.java
+++ src/test/java/com/georent/service/DescriptionSearchServiceTest.java
@@ -1,23 +1,66 @@
 package com.georent.service;
 
-import org.junit.Before;
+import com.georent.dto.DescriptionDTO;
+import com.georent.dto.LotPageDTO;
+import com.georent.dto.LotPageable;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.persistence.EntityManagerFactory;
+import java.util.List;
+import java.util.Set;
 
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class DescriptionSearchServiceTest {
 
+    private final int pageNumber = 1;
+    private final int count = 3;
+    private final String methodPage = "next";
+    private final String searchTerm = "Kiev";
+    private final String lotName = "lotName2";
 
-    private DescriptionSearchService descriptionSearchService;
-    private EntityManagerFactory entityManagerFactory;
-    private LotService lotService;
-    private AWSS3Service awss3Service = mock(AWSS3Service.class);
+    @Autowired
+    DescriptionSearchService descriptionSearchService;
 
+    @Test
+    void descriptionSearchServiceSuccessfullyStarted() {
+        assertThat(descriptionSearchService != null);
+    }
 
-    @Before
-    public void setup() {
-        descriptionSearchService = new DescriptionSearchService(entityManagerFactory, lotService, awss3Service);
+    @Test
+    void whenFuzzyLotSearchSuccessfully_Return_ListDescriptionDTO() {
+        List<DescriptionDTO> actualLotDTOS = descriptionSearchService.fuzzyLotSearch(searchTerm);
+        assertThat(actualLotDTOS != null);
+
+    }
+
+    @Test
+    void whenFuzzyLotNameAndAddressSearchSuccessfully_Return_SetLotPageDTO() {
+
+        Set<LotPageDTO> actualPageDTO = descriptionSearchService.fuzzyLotNameAndAddressSearch(searchTerm, lotName);
+        assertThat(actualPageDTO != null);
+    }
+
+    @Test
+    void whenfuzzyLotPageNameAndAddressSearchSuccessfully_Return_LotPageable() {
+        LotPageable actualLotPageable = descriptionSearchService.fuzzyLotPageNameAndAddressSearch(
+                pageNumber,
+                count,
+                methodPage,
+                searchTerm,
+                lotName);
+        assertThat(actualLotPageable != null);
+        Assert.assertEquals(0, actualLotPageable.getTotalPages());
+        Assert.assertEquals(1, actualLotPageable.getPageNumber());
     }
 
 }
+
+
+
