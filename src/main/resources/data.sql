133265f9faf226350743388f303931c8a3cf7c58
oleg_kosenkov <o.kosenkov357@gmail.com>
2019-07-13 01:11:48 +0300

    GEOR-55 Database populator file data.sql deleted.

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO geo_rent_user (id, email, first_name, last_name, password, phone_number)
-   VALUES (1, 'oleg@gmail.com', 'Oleg', 'Kosenkov', '$2a$10$Oa2Wby6Y/FRnf.Y4sdzBne8qoxWjDvyxhYpXnzScWdoKR5p8eOl6.', '123456789');
